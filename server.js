b41f4de6a87483ae23911650d61e04957de18542
vydang-dayone <vydang@dayoneteams.com>
2019-12-16 08:43:21 +0700

    feat: intro page

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-const app = require('express')();
+const express = require('express');
+
+const app = express();
 const http = require('http').createServer(app);
 
-const homePage = '/pages/home.html';
+const introPage = '/pages/intro.html';
+const setupPage = '/pages/setup.html';
 
+
+app.use('/static', express.static('public'));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + homePage);
+  res.sendFile(__dirname + introPage);
 });
+
+app.get('/setup', function(req, res){
+  res.sendFile(__dirname + setupPage);
+});
+
+
 
 http.listen(3000, () => {
   console.log('Server is running at http://localhost:3000')
