7b8970ee1199ee9cbb7110c7aec25a52e3dfa349
vydang-dayone <vydang@dayoneteams.com>
2019-12-16 10:32:11 +0700

    feat: layot page

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const http = require('http').createServer(app);
 
 const introPage = '/pages/intro.html';
 const setupPage = '/pages/setup.html';
+const layoutPage = '/pages/layout.html';
+const contentPage = '/pages/content.html';
+const componentsPage = '/pages/components.html';
+const utilitiesPage = '/pages/utilities.html';
+const extendPage = '/pages/extend.html';
 
 
 app.use('/static', express.static('public'));
@@ -17,6 +22,25 @@ app.get('/setup', function(req, res){
   res.sendFile(__dirname + setupPage);
 });
 
+app.get('/layout', function(req, res){
+  res.sendFile(__dirname + layoutPage);
+});
+
+app.get('/content', function(req, res){
+  res.sendFile(__dirname + contentPage);
+});
+
+app.get('/components', function(req, res){
+  res.sendFile(__dirname + componentsPage);
+});
+
+app.get('/utilities', function(req, res){
+  res.sendFile(__dirname + utilitiesPage);
+});
+
+app.get('/extend', function(req, res){
+  res.sendFile(__dirname + extendPage);
+});
 
 
 http.listen(3000, () => {
